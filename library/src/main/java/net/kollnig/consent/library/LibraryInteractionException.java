da916e9f261fbebae91cb0f71e9ec5fe40b6c434
LucifersBunny <187947442+LucifersBunny@users.noreply.github.com>
2025-01-03 03:48:19 +0000

    Update LibraryInteractionException.java

--- library/src/main/java/net/kollnig/consent/library/LibraryInteractionException.java
+++
@@ -1,7 +0,0 @@
-package net.kollnig.consent.library;
-
-public class LibraryInteractionException extends Exception {
-    public LibraryInteractionException(String message) {
-        super(message);
-    }
-}
